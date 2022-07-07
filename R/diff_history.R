7f37fc0bea6e863d6060b3c78eedb29387f93a5d
martinmodrak <modrak.mar@gmail.com>
2022-07-07 16:42:49 +0200

    Updates to non-mon example

--- R/diff_history.R
+++ R/diff_history.R
@@ -18,14 +18,34 @@ get_precomputed_gamma_thresholds <- function(K, min_sims = 1, max_sims = 1000) {
   gamma_file <- paste0("./_SBC_cache/gamma_thresholds_",K,"_",min_sims,"_", max_sims, ".rds")
   if(!file.exists(gamma_file)) {
     message("Precomputing gamma thresholds")
+    if(max_sims > 1000) {
+      sims_above_1000 <- round(exp(seq(log(1001), log(max_sims), by = 0.02)))
+      #Ensure max_sims is present
+      sims_above_1000 <- unique(c(sims_above_1000, max_sims))
+      if(min_sims <= 1000) {
+        sims_to_compute <- c(min_sims:1000, sims_above_1000)
+      } else {
+        sims_to_compute <- sims_above_1000
+      }
+      interpolate <- TRUE
+    } else {
+      sims_to_compute <- min_sims:max_sims
+      interpolate <- FALSE
+    }
     # Computing time differs by N, randomly permute to get optimal division of work
-    N_sims <- sample(min_sims:max_sims)
-    thresholds <- future.apply::future_sapply(N_sims, FUN = function(n_sims) {
+    sims_to_compute <- sample(sims_to_compute)
+    thresholds <- future.apply::future_sapply(sims_to_compute, FUN = function(n_sims) {
       SBC:::adjust_gamma(N = n_sims, L = 1, K = K)
     })
 
-    thresholds_df <-
-      dplyr::arrange(data.frame(N_sims = N_sims, log_gamma_threshold = log(thresholds)), N_sims)
+
+    if(interpolate) {
+      thres_approx <- approx(log(sims_to_compute), log(thresholds), xout = log(min_sims:max_sims))
+      thresholds_df <- data.frame(N_sims = min_sims:max_sims, log_gamma_threshold = thres_approx$y)
+    } else {
+      thresholds_df <-
+        dplyr::arrange(data.frame(N_sims = sims_to_compute, log_gamma_threshold = log(thresholds)), N_sims)
+    }
 
     saveRDS(thresholds_df, gamma_file)
   } else {
@@ -66,30 +86,46 @@ compute_ks_test_history <- function(ranks, max_rank) {
 }
 
 
-plot_log_gamma_history <- function(results, min_sim_id = 0, max_sim_id = Inf) {
+plot_log_gamma_history <- function(results, min_sim_id = 0, max_sim_id = Inf, wrap_cols = 4, variables_regex = NULL, ylim = NULL) {
   unique_max_rank <- unique(results$stats$max_rank)
   if(length(unique_max_rank) > 1) {
     stop("Requires all max_rank to be equal")
   }
-  gamma_thresholds_df <- get_precomputed_gamma_thresholds(K = unique_max_rank + 1)
-  results$stats %>%
+
+  max_sim_id_to_show <- min(max_sim_id, max(results$stats$sim_id))
+
+  gamma_thresholds_df <- get_precomputed_gamma_thresholds(K = unique_max_rank + 1,
+                                                          min_sims = 1, max_sims = max(max_sim_id_to_show, 1000))
+
+  stats <- results$stats
+  if(!is.null(variables_regex)) {
+     stats <- stats %>% filter(grepl(variables_regex, variable))
+  }
+
+  stats %>%
+    filter(sim_id <= max_sim_id) %>%
     group_by(variable) %>%
     mutate(log_gamma = compute_log_gamma_history(rank, unique_max_rank)) %>%
-    filter(sim_id >= min_sim_id, sim_id <= max_sim_id) %>%
+    filter(sim_id >= min_sim_id) %>%
     inner_join(gamma_thresholds_df, by = c("sim_id" = "N_sims")) %>%
-    ggplot(aes(x = sim_id, y = log_gamma - log_gamma_threshold, color = variable)) +
-    geom_hline(yintercept = 0, color = "black") +
+    ggplot(aes(x = sim_id, y = log_gamma - log_gamma_threshold)) +
+    geom_hline(yintercept = 0, color = "lightblue") +
     geom_line() +
-    scale_y_continuous("Log Gamma - Threshold")
+    scale_y_continuous("Log Gamma - Threshold", limits = ylim) +
+    scale_x_continuous("Number of simulations") +
+    facet_wrap(~variable, ncol = wrap_cols)
 }
 
-plot_ks_test_history <- function(results, min_sim_id = 0, max_sim_id = Inf) {
+plot_ks_test_history <- function(results, min_sim_id = 0, max_sim_id = Inf, wrap_cols = 4, ylim = NULL) {
   results$stats %>%
+    filter(sim_id <= max_sim_id) %>%
     group_by(variable) %>%
     mutate(ks_p = compute_ks_test_history(rank, unique(max_rank))) %>%
-    filter(sim_id >= min_sim_id, sim_id <= max_sim_id) %>%
-    ggplot(aes(x = sim_id, y = ks_p, color = variable)) +
-    geom_hline(yintercept = 0.05, color = "black") +
+    filter(sim_id >= min_sim_id) %>%
+    ggplot(aes(x = sim_id, y = ks_p)) +
+    geom_hline(yintercept = 0.05,  color = "lightblue") +
     geom_line() +
-    scale_y_log10("P - value (KS test)")
+    scale_y_log10("P - value (KS test)", limits = ylim) +
+    scale_x_continuous("Number of simulations") +
+    facet_wrap(~variable, ncol = wrap_cols)
 }
